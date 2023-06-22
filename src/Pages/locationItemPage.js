6550fb889a9189f6ddc4830b901df8ba33350b61
Charles Stello <cgissell2012@gmail.com>
2023-06-21 22:39:40 -0400

    0.1.2 - UI Tweaks - Added logo to top left of all pages to have consistent branding - Fixed input backgrounds not changing for dark mode on the add item modal

--- src/Pages/locationItemPage.js
+++ src/Pages/locationItemPage.js
@@ -6,6 +6,8 @@ import "./Styles/locationItemPage.css";
 import { useTheme } from "../Utils/Themes/theme";
 
 // Components
+import pantraLight from "../Assets/Images/pantraHorizontalLight.svg";
+import pantraDark from "../Assets/Images/pantraHorizontalDark.svg";
 import { ItemList } from "../Components/itemList/itemList";
 
 export const LocationItemPage = (props) => {
@@ -20,6 +22,8 @@ export const LocationItemPage = (props) => {
 
     return (
         <div className={`locationItemPage ${theme}`}>
+            <div className="logo"><img src={theme === "lightTheme" ? pantraLight : pantraDark}/></div>
+
             <div className="backBtn" onClick={handleBackClick}>
                 <i className="bx bx-arrow-back"/><span>Back</span>
             </div>
